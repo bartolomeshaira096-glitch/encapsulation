a4eb327cfa77ba51b70d3bac57ab3a7a88dad481
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-30 14:34:00 +0800

    initialize Fan instances in TestFan class, set properties using encapsulated setter methods, and implement get_fans method for object retrieval

--- fan_management_system/test_fan.py
+++ fan_management_system/test_fan.py
@@ -7,3 +7,12 @@ class TestFan:
         self.fan1.set_radius(10)
         self.fan1.set_color("yellow")
         self.fan1.set_on(True)
+
+        self.fan2 = Fan()
+        self.fan2.set_speed(Fan.MEDIUM)
+        self.fan2.set_radius(5)
+        self.fan2.set_color("blue")
+        self.fan2.set_on(False)
+
+    def get_fans(self):
+        return self.fan1, self.fan2
