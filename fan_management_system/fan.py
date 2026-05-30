7b066886dec40c2a875f7067909c143badfe1b01
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-30 14:15:08 +0800

    feat: add Fan class with private fields, default constructor, getters, and fan speed constants

--- fan_management_system/fan.py
+++ fan_management_system/fan.py
@@ -2,4 +2,21 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
-    
+
+    def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
+
+     def get_speed(self):
+        return self.__speed
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_color(self):
+        return self.__color
+
+    def is_on(self):
+        return self.__on
