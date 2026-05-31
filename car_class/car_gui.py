7aabf6a315897d7672b66926bd1b1d2b2eb0c606
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-31 16:42:00 +0800

    implement responsive background image resizing

--- car_class/car_gui.py
+++ car_class/car_gui.py
@@ -14,7 +14,7 @@ class CarGUI:
         self.root.geometry("1000x650")
         self.root.minsize(900, 600)
 
-        self.original_bg = Image.open("ferrari_bg.jpg")
+        self.original_bg = Image.open("car_class/ferrari_bg.jpg")
 
         self.bg_label = tk.Label(self.root)
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
@@ -25,49 +25,68 @@ class CarGUI:
 
         self.root.mainloop()
 
+    def resize_bg(self, event=None):
+
+        width = self.root.winfo_width()
+        height = self.root.winfo_height()
+
+        resized = self.original_bg.resize(
+            (width, height),
+            Image.Resampling.LANCZOS
+        )
+
+        self.bg_photo = ImageTk.PhotoImage(resized)
+
+        self.bg_label.config(image=self.bg_photo)
+
     def setup_ui(self):
 
         title = tk.Label(
             self.root,
             text="FERRARI 296 GTS",
             font=("Montserrat", 34, "bold"),
-            bg="#0f0f0f",
+            bg="black",
             fg="#ff2e2e"
         )
         title.pack(pady=20)
+        title.lift()
 
         subtitle = tk.Label(
             self.root,
             text="Luxury Performance Speed Simulator",
             font=("Segoe UI", 14),
-            bg="#0f0f0f",
+            bg="black",
             fg="white"
         )
         subtitle.pack()
+        subtitle.lift()
 
         info = tk.Label(
             self.root,
             text=f"Year Model: {self.car.get_year_model()}",
             font=("Segoe UI", 14),
-            bg="#0f0f0f",
+            bg="black",
             fg="#cccccc"
         )
         info.pack(pady=10)
+        info.lift()
 
         self.speed_label = tk.Label(
             self.root,
             text="0 KM/H",
             font=("Impact", 60),
-            bg="#0f0f0f",
+            bg="black",
             fg="#00ff99"
         )
         self.speed_label.pack(pady=50)
+        self.speed_label.lift()
 
         button_frame = tk.Frame(
             self.root,
-            bg="#0f0f0f"
+            bg="black"
         )
         button_frame.pack()
+        button_frame.lift()
 
         accelerate_btn = tk.Button(
             button_frame,
