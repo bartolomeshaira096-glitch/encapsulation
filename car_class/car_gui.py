5f7c452ee5de8bff0bc05c1d66add49b93758564
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-31 15:39:15 +0800

    create CarGUI class and initialize main window

--- car_class/car_gui.py
+++ car_class/car_gui.py
@@ -2,3 +2,19 @@ import tkinter as tk
 from tkinter import ttk
 from car import Car
 
+class CarGUI:
+    def __init__(self):
+
+        self.car = Car(2025, "Ferrari 296 GTS")
+
+        self.root = tk.Tk()
+        self.root.title("Ferrari GTS Speed Simulator")
+
+        self.root.geometry("1000x650")
+        self.root.minsize(900, 600)
+
+        self.root.configure(bg="#0f0f0f")
+
+        self.setup_ui()
+
+        self.root.mainloop()
