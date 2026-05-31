7658b400f427561d4bbcaf60dd9ec3b31c6affe7
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-31 17:31:15 +0800

    add background image and centered floating frame for layout styling

--- pet_class/pet_gui.py
+++ pet_class/pet_gui.py
@@ -10,3 +10,10 @@ class PetGUI:
         self.window.title("🐾 Pet Information System")
         self.window.geometry("600x400")
         self.window.resizable(True, True)
+
+        self.bg_image = tk.PhotoImage(file="pet_bg.png")
+        self.bg_label = tk.Label(self.window, image=self.bg_image)
+        self.bg_label.place(relwidth=1, relheight=1)
+
+        self.frame = tk.Frame(self.window, bg="#000000", bd=0)
+        self.frame.place(relx=0.5, rely=0.5, anchor="center")
